48ee99d3f9e5fa3fe218cd127187f192619b34bb
jacuchilla <jcuchilla.csupomona@gmail.com>
2017-04-04 15:38:06 -0700

    View lyrics functionality integration

--- src/app/music/music.component.ts
+++ src/app/music/music.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-music',
   templateUrl: './music.component.html',
   styleUrls: ['./music.component.css']
 })
+
+
+
+
+
 export class MusicComponent implements OnInit {
 
-  constructor() { }
+
+songsList: any;
+songsListShow: any;
+
+  constructor() { 
+    this.songsList = ["Surfwood", "Carnies", "Horchata", "Mary Jane", "Away It Goes", "Square Peg"];
+    this.songsListShow = [true, true, true, false, true, true];
+   }
 
   ngOnInit() {
+  }
+
+  songClick(event, i, index) {
+    console.log("this the event ", event.target, i.songsList, index, i.songsList[index]);
+    // this.songsListShow[index] = false;
+    if (this.songsListShow[index]) {
+      this.songsListShow[index] = false;
+    }
+    else {
+      this.songsListShow[index] = true;
+    }
+
+    // console.log("this the attributes ", event.target.attributes);
+    // console.log("this the attributes ", event.target.attributes.id.nodeValue);
+    // "showLyrics = showLyrics ? false : true"
   }
 
 }
