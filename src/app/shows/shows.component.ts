20da2a1a6b570a4e43b106184544b7cea3cc8b2d
jacuchilla <jcuchilla.csupomona@gmail.com>
2017-04-04 16:57:14 -0700

    Fan page component integration

--- src/app/shows/shows.component.ts
+++ src/app/shows/shows.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowsComponent implements OnInit {
 
-  constructor() { }
+  flyers: any;
+
+  constructor() { 
+    this.flyers = [{title: "Return of Ska", date: "3/23/17", url: "http://los2x4s.weebly.com/uploads/8/0/1/5/80156922/fb-img-1486869835996_orig.jpg"}, {title: "The Great 8 Presents", date: "1/23/17", url: "http://los2x4s.weebly.com/uploads/8/0/1/5/80156922/fb-img-1484182580139_orig.jpg"}]
+  }
 
   ngOnInit() {
   }
