6b644c95681fc0a6e2daf3baa7a253d2c7a84777
jacuchilla <jcuchilla.csupomona@gmail.com>
2017-04-05 20:47:18 -0700

    bio restructure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to Los 2x4s!';
+  title = 'Los 2x4s Logoholder';
 }
